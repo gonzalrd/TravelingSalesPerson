e498f9d8d3a533729f95338483d44b5aa94f4d84
Raquel Gonzalez <gonzalrd@muohio.edu>
2012-11-30 12:37:06 -0800

    Got TSP method to build and return results
    problem with the lenght of the list being returned

--- TravelingSalesPerson/GraphAlgs.cpp
+++ TravelingSalesPerson/GraphAlgs.cpp
@@ -3,27 +3,49 @@
 
 
 
-
+std::vector<NodeID> curIds;
 //solves the traveling salesperson problem defined it its .h file
 std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G){
+curIds = std::vector<NodeID>();
 
-std::pair<std::vector<NodeID>, EdgeWeight> bestTour = std::pair<std::vector<NodeID>, EdgeWeight>();
+EdgeWeight best = BestTour(G, 0);
 
-//TODO: write this method
+
+std::pair<std::vector<NodeID>, EdgeWeight> bestTour = make_pair(curIds, best);
+ 
 return bestTour;
 }
+ EdgeWeight BestTour(Graph* G, int startPoint){
 
-//::pair<std::vector<NodeID>, EdgeWeight> BestTour(Graph* G, int startPoint){
+	std::vector<NodeID> Ids = std::vector<NodeID>();
 
-//std::pair<std::vector<NodeID>, EdgeWeight> bestTour = std::pair<std::vector<NodeID>, EdgeWeight>();
-			//if(G->size()-startPoint ==1){
-				//return bestTour;
-			//}
-			//else
-			//	for(int i = startPoint; i<G->size(); i++){
+	EdgeWeight cur = 0.0;
+			
+		if(G->size()-startPoint == 1){
+				return cur;
+			}
+			else
+		//this method needs to loop back around to get all of the points
+		for(int i = startPoint; i<G->size(); i++){
+			if(i != G->size()-1){//check for last one so you dont go out of bounds
+				 cur = cur + G->weight(i , i+1);
+				 curIds.push_back(i);
+			}
+			
+			}
+		for(int j = 0; j < startPoint; j++){
+			cur = cur + G->weight(j , j+1);
+			curIds.push_back(j);
+		}
 
+		EdgeWeight best = BestTour(G, startPoint+1);
 
-			//	}
+		if(best > cur){
+			best = cur;
+			curIds = Ids;
+			return cur;
+		}
 
+		else return BestTour(G, startPoint+1);
 
-//}
+}
--- TravelingSalesPerson/GraphAlgs.h
+++ TravelingSalesPerson/GraphAlgs.h
@@ -21,4 +21,6 @@
  */
 
 std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G);
-std::pair<std::vector<NodeID>, EdgeWeight> BestTour(Graph* G, int startPoint);//recursive method that solves problem in TSP
+EdgeWeight BestTour(Graph* G, int startPoint);//recursive method that solves problem in TSP
+
+
